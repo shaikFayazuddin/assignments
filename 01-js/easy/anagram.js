8e6212b2819c2a84da9a9364719171f73ec36d05
shaikFayazuddin <shaikfayazuddin18@gmail.com>
2023-12-06 23:32:37 +0530

    anagram solved

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,14 @@
 */
 
 function isAnagram(str1, str2) {
-
+    let newStr1 = str1.replace(/\s/g, "").toLowerCase().split("").sort().join("")
+    console.log(newStr1)
+    let newStr2 = str2.replace(/\s/g, "").toLowerCase().split("").sort().join("")
+    if(newStr1 === newStr2){
+      return true
+    }else{
+      return false
+    }
 }
-
+console.log(isAnagram('Debit Card', 'Bad Credit'))
 module.exports = isAnagram;
