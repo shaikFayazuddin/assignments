2c00bfb844b9807c30d264f03b40b68098913bcd
shaikFayazuddin <shaikfayazuddin18@gmail.com>
2023-12-07 10:02:31 +0530

    solved calculate time question

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,6 +8,47 @@ Hint - use Date class exposed in JS
 There is no automated test for this one, this is more for you to understand time goes up as computation goes up
 */
 
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+
+var start = new Date().getMilliseconds()
+
+
 function calculateTime(n) {
-    return 0.01;
+    let sum = 0
+    for(let i=1;i<=n;i++){
+        sum += i
+    }
+    return sum;
 }
+console.log(calculateTime(10000000))
+var end = new Date().getMilliseconds()
+
+
+let timeTaken = end -start
+console.log(timeTaken + " milli seconds")
+
+
+//or it can also be done using performance.now() 
+
+/*** 
+var start = performance.now()
+
+
+function calculateTime(n) {
+    let sum = 0
+    for(let i=1;i<=n;i++){
+        sum += i
+    }
+    return sum;
+}
+
+console.log(calculateTime(10000000))
+
+var end = performance.now()
+
+
+let timeTaken = end -start
+console.log(timeTaken + " milli seconds")
+*/
