89aebabdbd3d6480c29e9580b19ee396a93edc21
shaikFayazuddin <shaikfayazuddin18@gmail.com>
2023-12-07 21:28:17 +0530

    todo-list solved

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,42 @@
 */
 
 class Todo {
+  constructor(){
+    this.task = []
+  }
+  add(todo){
+    this.task.push(todo)
+  }
+  remove(indexOfTodo){
+    this.task.splice(indexOfTodo,1)
+  }
+  update(index,updatedTodo){
+    if(index>=0 && index<this.task.length){
+      this.task[index] = updatedTodo
+    }
+  }
+  getAll(){
+    return this.task
+  }
+  get(indexOfTodo){
+    if(indexOfTodo>=0 && indexOfTodo<this.task.length){
+      return this.task[indexOfTodo]
+    }else{
+      return null
+    }
+  }
+  clear(){
+    this.task.splice(0,this.task.length)
+  }
+
 
 }
-
+let fayaztodo = new Todo()
+fayaztodo.add("sleep")
+fayaztodo.add("eat")
+fayaztodo.remove(1)
+fayaztodo.update(1,"play")
+console.log(fayaztodo.getAll())
+console.log(fayaztodo.get(4))
+console.log(fayaztodo)
 module.exports = Todo;
