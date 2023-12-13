6128b3134f0682438b8049dd6515f35fd5843d84
shaikFayazuddin <shaikfayazuddin18@gmail.com>
2023-12-13 12:36:03 +0530

    first hard problem solved

--- week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
+++ week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -3,6 +3,35 @@
 */
 
 function wait(n) {
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve()
+        }, n*1000);
+    })
 }
+let ans = wait(5000)
+ans.then(function(){
+    console.log("Promise resolved")
+})
+
 
 module.exports = wait;
+
+// const p1 = new Promise((resolve,reject)=>{ setTimeout(()=> resolve("Promise got resolved"),5000)})
+
+// const p2 = new Promise((resolve,reject)=>{ setTimeout(()=> resolve("Promise got resolved"),10000)})
+
+
+// async function wow(){
+//     console.log("hello world")
+
+//     const val = await p1
+//     console.log("fay1")
+//     console.log(val)
+
+//     const val2= await p2
+//     console.log("byw")
+//     console.log(val2)
+// }
+
+// wow()
