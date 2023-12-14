a546601b16b12ca6e7ebd315da090250eec267c3
shaikFayazuddin <shaikfayazuddin18@gmail.com>
2023-12-14 22:53:08 +0530

    solved second question

---
+++ week-2/01-async-js/easy/2-counter.js
@@ -0,0 +1,18 @@
+//Counter without setInterval
+
+//Without using setInterval, try to code a counter in Javascript. There is a hint at the bottom of the file if you get stuck.
+
+function counter(n) {
+    let cntr = 0;
+    function printCounter() {
+        if(cntr<=n){
+//            console.clear()
+            console.log(cntr + " seconds")
+            setTimeout(printCounter,1000)
+        }
+    cntr++
+    }
+    printCounter()
+  }
+counter(5);
+  
