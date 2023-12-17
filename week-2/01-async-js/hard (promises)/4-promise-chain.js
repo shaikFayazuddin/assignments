c3b638fe93f1787892a8b23749fd48110dca4743
shaikFayazuddin <shaikfayazuddin18@gmail.com>
2023-12-17 16:41:14 +0530

    promise chain resolved

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -6,19 +6,40 @@
  */
 
 function wait1(t) {
-
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve("First Promise resolved")
+        },t*1000)
+    })
 }
 
 function wait2(t) {
-
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve("Second Promise resolved")
+        },t*1000)
+    })
 }
 
 function wait3(t) {
-
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve("Third Promise resolved")
+        },t*1000)
+    })
 }
 
 function calculateTime(t1, t2, t3) {
-
+    const start = new Date()
+    return wait1(t1).then(()=>{
+        return wait2(t2).then(()=>{
+            return wait3(t3).then(()=>{
+                const end = new Date()
+                const timeConsumed = end -start
+                return timeConsumed
+            })
+        })
+    })
 }
 
 module.exports = calculateTime;
